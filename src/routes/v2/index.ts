5f8c002e47eb67de6e8a14abd7b16a41cfa18444
harsh <harshsondagar@weetechsolution.com>
2026-06-24 12:14:17 +0530

    feat(user V2): add auth and user route

--- src/routes/v2/index.ts
+++ src/routes/v2/index.ts
@@ -1,9 +1,11 @@
 import router from "express"
+import authRouter from "./auth.router"
+import userRouter from "./user.router"
 
 
 const v2Router = router()
 
-v2Router.use("/auth")
-v2Router.use("/user")
+v2Router.use("/auth",authRouter)
+v2Router.use("/user",userRouter)
 
 export default v2Router
